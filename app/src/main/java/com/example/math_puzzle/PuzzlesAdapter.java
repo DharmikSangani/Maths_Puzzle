b3cfd97be9108b3335cebab691c3aa2608c662ff
dharmik <dharmiksangani2005@gmail.com>
2023-06-16 10:32:08 +0530

    four Commit

--- app/src/main/java/com/example/math_puzzle/PuzzlesAdapter.java
+++ app/src/main/java/com/example/math_puzzle/PuzzlesAdapter.java
@@ -49,37 +49,37 @@ public class PuzzlesAdapter extends BaseAdapter
     @Override
     public View getView(int position, View convertview, ViewGroup parent) {
         convertview=LayoutInflater.from(puzzles_secondpage_activity).inflate(R.layout.activity_puzzles_secondpage_item,parent,false);
-        ImageView imageView=convertview.findViewById(R.id.lockno);
-        imageView.setImageResource(config.lock);
+        ImageView lock=convertview.findViewById(R.id.lockno);
+        lock.setImageResource(config.lock);
 
-//        String page = preferences.getString("page","p");
-//
-//        if (page.equals("p")){
-//            lock = convertView.findViewById(R.id.lockon);
-//            tick = convertview.findViewById(R.id.tick);
-//            textView = convertview.findViewById(R.id.lockno);
-//            String status = preferences.getString("lastlevel"+(position+1),"levelNo");
-//            levelNo = preferences.getInt("levelNo",0);
-//            textView.setText("");
-//
-//            if (status.equals("win")){
-//                lock.setVisibility(View.INVISIBLE);
-//                textView.setText(""+no[position]);
-//                tick.setImageResource(R.drawable.tick);
-//            }
-//            else if (status.equals("skip") || levelNo==position){
-//                lock.setVisibility(View.INVISIBLE);
-//                textView.setText(""+no[position]);
-//            }
-//            textView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Intent intent = new Intent(puzzles_secondpage_activity, Continue_SecondPage_Activity.class);
-//                    intent.putExtra("levelNo",position);
-//                    puzzles_secondpage_activity.startActivity(intent);
-//                }
-//            });
-//        }
+        String page = preferences.getString("page","p");
+
+        if (page.equals("p")){
+
+            tick = convertview.findViewById(R.id.tick);
+            textView = convertview.findViewById(R.id.textno);
+            String status = preferences.getString("lastlevel"+(position+1),"levelNo");
+            levelNo = preferences.getInt("levelNo",0);
+            textView.setText("");
+
+            if (status.equals("win")){
+                lock.setVisibility(View.INVISIBLE);
+                textView.setText(""+no[position]);
+                tick.setImageResource(R.drawable.tick);
+            }
+            else if (status.equals("skip") || levelNo==position){
+                lock.setVisibility(View.INVISIBLE);
+                textView.setText(""+no[position]);
+            }
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(puzzles_secondpage_activity, Continue_SecondPage_Activity.class);
+                    intent.putExtra("levelNo",position);
+                    puzzles_secondpage_activity.startActivity(intent);
+                }
+            });
+        }
         return convertview;
     }
 }
